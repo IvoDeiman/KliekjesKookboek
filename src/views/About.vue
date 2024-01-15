78589e7da61be71713354536113e128bb441db63
AdamHasa <adamhasanin@hotmail.nl>
2024-01-15 14:44:13 +0100

    about us page update

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,46 +1,105 @@
 <template>
-  <div style="padding-top: 100px">
-    <h1 style="font-size: 70px">Wie zijn wij</h1>
-    <p>Even voorstellen, wij zijn het team achter Het Kliekjes Kookboek.
-    Wij zijn een jonge start-up opgericht door een groep jonge gemotiveerde
-    studenten van de Hanzehogeschool te Groningen.
+  <div class="about-us-container">
+    <section class="section-introduction">
+      <div class="section-bg">
+      <h1 class="section-title">Wie zijn wij</h1>
+        <p>
+          Even voorstellen, wij zijn het team achter Het Kliekjes Kookboek. Wij
+          zijn een jonge start-up opgericht door een groep jonge gemotiveerde
+          studenten van de Hanzehogeschool te Groningen.
+        </p>
+        <p>
+          Wij hebben elkaar leren kennen op de opleiding HBO-ICT toen we in jaar
+          4 in dezelfde klas kwamen. Wat begon als een idee voor een project is
+          inmiddels uitgegroeid tot werkelijkheid en werken wij full-time met
+          enthousiasme en passie aan ons product.
+        </p>
+      </div>
+    </section>
 
-    Wij hebben elkaar leren kennen op de opleiding HBO-ICT toen we in jaar 4
-    in dezelfde klas kwamen. Wat begon als een idee voor een project is inmiddels
-    uitgegroeid tot werkelijkheid en werken wij full-time met enthousiasme en passie aan ons product.</p>
+    <section class="section-product">
+      <div class ="section-bg">
+        <h1 class="section-title">Wat is ons product</h1>
+        <p>
+          Het Kliekjes Kookboek is een online receptenboek. Deze kan gebruikt
+          worden om nieuwe recepten te leren, informatie in te winnen of te
+          filteren op overgebleven kliekjes zodat deze in plaats van verspild
+          worden gebruikt gaan worden in een van de vele heerlijke recepten die
+          wij aanbieden.
+        </p>
+      </div>
+    </section>
 
-    <h1 style="font-size: 70px">Wat is ons product</h1>
-    <p>Het Kliekjes Kookboek is een online receptenboek. Deze kan gebruikt worden
-    om nieuwe recepten te leren, informatie in te winnen of te filteren op overgebleven
-    kliekjes zodat deze in plaats van verspild worden gebruikt gaan worden in een
-    van de vele heerlijke recepten die wij aanbieden.</p>
+    <section class="section-why">
+      <div class ="section-bg">
+        <h1 class="section-title">Waarom</h1>
+        <p>
+          "Gemiddeld verspilden Nederlanders in 2022 thuis 33,4 kg eten per
+          persoon. Binnen de hele voedselketen zijn van boer tot bord weliswaar
+          veelbelovende initiatieven om verspilling tegen te gaan, maar meer inzet
+          is nodig. Nog altijd wordt ongeveer een kwart van al het voedsel in
+          Nederland verspild." Bron ~rijksoverheid
+        </p>
+        <p>
+          "Niemand kookt korter dan de Nederlander " Bron ~Trouw
+        </p>
+        <p>
+          Omdat wij van Het Kliekjes Kookboek vooruit willen kijken naar de
+          toekomst pakken wij graag het probleem van voedselverspilling aan. Zoals
+          eerder genoemd is het mogelijk om op onze site met overgebleven kliekjes
+          recepten te vinden. Verder vinden wij het jammer om te lezen dat
+          Nederlanders een relatief kleine culinaire horizon hebben. Ook hierbij
+          willen wij ons van Het Kliekjes Kookboek inzetten om dit getal te
+          vermenigvuldigen en zo de mensen kennis te laten maken met andere
+          heerlijke keuken!
+        </p>
+    </div>
+    </section>
 
-    <h1 style="font-size: 70px">Waarom</h1>
-    <p>"Gemiddeld verspilden Nederlanders in 2022 thuis 33,4 kg eten per persoon. Binnen de hele
-      voedselketen zijn van boer tot bord weliswaar veelbelovende initiatieven om verspilling
-      tegen te gaan, maar meer inzet is nodig. Nog altijd wordt ongeveer een kwart van
-      al het voedsel in Nederland verspild." Bron ~rijksoverheid</p><br>
-      <p>"De gemiddelde Nederlander maakt in een heel jaar tijd week in week uit dezelfde 7 recepten" Bron ~Adam Hasanin
-      </p><br>
-    <p>
-      Omdat wij van Het Kliekjes Kookboek vooruit willen kijken naar de toekomst pakken wij graag het probleem van voedselverspilling aan.
-      Zoals eerder genoemd is het mogelijk om op onze site met overgebleven kliekjes recepten te vinden.
-      Verder vinden wij het jammer om te lezen dat Nederlanders een relatief kleine culineare horizon hebben.
-      Ook hierbij willen wij ons van Het Kliekjes Kookboek inzetten om dit getal te veelvoudigen en zo de mensen kennis
-      te laten maken met andere heerlijke keuken!</p>
-
-    <h1 style="font-size: 70px">Motto</h1>
-    <p>Carpe diem sugondese nuts lmao</p>
-    <br>
-    <br>
+    <section class="section-motto">
+      <div class ="section-bg">
+        <h1 class="section-title">Motto</h1>
+        <p>Heb je kliekjes en weet je niet wat je ermee moet? Zoek nu een lekker recept op gebaseerd op je restanten en maak iets nieuws van iets ouds! </p>
+      </div>
+    </section>
   </div>
 </template>
+
+<style scoped>
+.about-us-container {
+  max-width: 800px;
+  margin: 100px auto 20px; 
+  padding: 20px;
+  font-family: 'Bitter', sans-serif;
+}
+
+.section-bg {
+  /* https://www.trouw.nl/nieuws/niemand-kookt-korter-dan-de-nederlander~bcb3d436/?referrer=https://www.google.com/*/
+  background-color: rgba(228, 164, 40, 0.3); 
+  border-radius: 10px; 
+  padding: 20px; 
+  margin-bottom: 20px;
+}
+
+.section-title {
+  font-size: 2em;
+  margin-bottom: 10px;
+}
+
+.section-introduction p,
+.section-product p,
+.section-why p,
+.section-motto p {
+  line-height: 1.6;
+  margin-bottom: 15px;
+}
+</style>
 
 <script>
 export default {
   name: "AboutUs",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
