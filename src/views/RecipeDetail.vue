38535529122d2a52a22c78e67753d23a0141448f
Zettios <77236807+Zettios@users.noreply.github.com>
2024-01-15 14:52:22 +0100

    Can now get info by id

--- src/views/RecipeDetail.vue
+++ src/views/RecipeDetail.vue
@@ -2,28 +2,27 @@
     <!--<h1 style="padding-top: 100px;">Recipe detail {{ id }}</h1>-->
     <div class="header h-[368px]">
       <div class="container-details sm:px-3 xl:px-[144px]">
-        <!--<div class="recipe-info custom-grid xl:h-[608px] xl:mb-8 lg:grid-cols-3 lg:gap-4 ">-->
         <div class="recipe-info lg:grid lg:grid-cols-3 lg:gap-4">
           <div class="recipe mb-8 lg:col-span-2 rounded-md box-shadow">
             <img src="../assets/oosterse-maaltijd.jpg" alt="maaltijd" class="w-full rounded-t-md h-[304px]" style="object-fit: cover;">
-            <h1 class="recipe-title">Gekruide kikkererwten en gegrilde courgette-taco's</h1>
-            <h2 class="quote"><i>“Vandaag op het menu: eet het op of sterf aan honger”</i></h2>
+            <h1 class="recipe-title">{{ title }}</h1>
+            <h2 class="quote"><i>{{ description }}</i></h2>
             <div class="grid info grid-rows-4">
               <div class="row">
                 <label class="font-bold">Bereidingstijd: </label>
-                <label>40 minuten</label>
+                <label>{{ preparationtime }}</label>
               </div>
               <div class="row">
                 <label class="font-bold">Score: </label>
-                <label>4/5</label>
+                <label>{{ rating }}</label>
               </div>
               <div class="row">
                 <label class="font-bold">Tags: </label>
                 <label></label>
               </div>
               <div class="row">
-                <label v-for="tag in tags" :key="tag.message">
-                  {{ tag.tag }}, 
+                <label v-for="tag in tags" :key="tag">
+                  {{ tag }}, 
                 </label>
               </div>
             </div>
@@ -31,7 +30,7 @@
 
           <div class="owner  mb-8 rounded-md box-shadow">
             <img src="../assets/Chef1.jpg" alt="chef" class="rounded-full owner-image md w-[304px] h-[304px]">
-            <label class="owner-name">Lettuce B. Frank</label>
+            <label class="owner-name">{{ owner }}</label>
             <hr class="owner-seperator">
             <p class="chef-info">Werkt al 10 jaar bij restaurant de Harmonie in Sexbierum, waarvan 4 als kok. Hiervoor werkte hij als vakkenvuller bij de locale Albert Heijn</p>
           </div>
@@ -43,8 +42,8 @@
           </div>
           <div class="">
             <ol>
-              <li v-for="ingredient in ingredients" :key="ingredient.ingredient" class="" style="">
-                <input type="checkbox" class="ingredient-checkbox"> {{ ingredient.ingredient }}
+              <li v-for="ingredient in ingredients" :key="ingredient" class="" style="">
+                <input type="checkbox" class="ingredient-checkbox"> {{ ingredient.name }} {{ ingredient.amount }} {{ ingredient.measurement }}
               </li>
             </ol>
           </div>
@@ -56,9 +55,10 @@
           </div>
           <div class="">
             <ol>
-              <li v-for="instruction in instructions" :key="instruction.instruction" class="instruction-step" style="">
-                {{ instruction.instruction }}
-              </li>
+              <!-- <li v-for="instruction in preparation" :key="instruction" class="instruction-step" style="">
+                {{ instruction }}
+              </li> -->
+              <li class="instruction-step" style="">{{ preparation }}</li>
             </ol>
           </div>
         </div>
@@ -69,46 +69,43 @@
 </template>
   
 <script>
+  import FirebaseService from "@/services/FirebaseService";
+
+  const fb = new FirebaseService();
   export default {
     name: "RecipeDetailPage",
     data(){
       return {
-        tags: [
-          { tag: 'Hoofdgerecht' }, 
-          { tag: 'Mexicaans' },
-          { tag: 'Vegan' },
-        ],
-        ingredients: [
-          { ingredient: '450 gram kikkererwten' }, 
-          { ingredient: '2 theelepels olijfolie' },
-          { ingredient: '0.5 theelepel chilipoeder' },
-          { ingredient: '0.25 theelepel paprikapoeder' },
-          { ingredient: '0.25 theelepel bruine suiker' },
-          { ingredient: '0.25 theelepel cacoa poeder' },
-          { ingredient: '0.25 theelepel gedroogde oregano' },
-          { ingredient: '0.25 theelepel kaneel' },
-          { ingredient: '0.125 theelepel komijn' },
-          { ingredient: '0.25 theelepel peper en zout' },
-          { ingredient: '2 grote courgettes' },
-          { ingredient: '1 eetlepel olijfolie' },
-          { ingredient: '1 theelepel zout en peper' },
-        ],
-        instructions: [
-          { instruction: 'Bereid de kikkererwten. Verwarm de oven voor op 400 ° F. Gooi de kikkererwten in olijfolie en kruiden en roer goed om te coaten. Verdeel de kikkererwten over een met bakpapier beklede bakplaat en bak ze 20 minuten, roer halverwege.' },
-          { instruction: 'Maak ondertussen de snel ingelegde radijsjes klaar. Roer de radijsjes, azijn, ahornsiroop en zout door elkaar in een kom en laat staan ​​terwijl je de rest klaarmaakt.' },
-          { instruction: 'Bereid de crema van Griekse yoghurt. Meng de Griekse yoghurt, het limoensap en ⅛ theelepel zout tot een gladde massa. Opzij zetten.'},
-          { instruction: 'Rooster de pepita’s. Verwarm in een middelgrote of grote pan op middelhoog vuur ½ theelepel olijfolie tot het heet is, maar niet rookt. Voeg de pepitas toe en rooster, af en toe roerend, tot ze licht geroosterd zijn, 2 tot 3 minuten. Leg de pepitas voorzichtig op een met keukenpapier bekleed bord, laat uitlekken en meng met ⅛ theelepel zout. Als alternatief kunt u in een mum van tijd gezouten geroosterde pepitas kopen!' },
-          { instruction: 'Verwarm een ​​grillpan voor op middelhoog vuur. Bestrijk de courgetteplakken aan beide kanten met olijfolie en bestrooi beide kanten rijkelijk met zout en peper. Als de grill heet is, gril je de courgette in een enkele laag tot ze aan beide kanten licht verkoold zijn, ongeveer 3 minuten per kant. Haal van het vuur.' },
-          { instruction: 'Bak de tortilla\'s lichtjes op een middelhoog vuur en draai ze één keer om, of verwarm elke tortilla in een droge pan tot ze net soepel zijn, ongeveer een minuut aan elke kant.' },
-          { instruction: 'Stel de taco\'s samen! Vul elke taco met gegrilde courgette, een lepel mol-gekruide kikkererwten, een scheutje Griekse yoghurtcrema, een handvol microgreens, snel gepekelde radijzen, verkruimelde feta en een snufje pepitas. Eet smakelijk!' },  
-        ],
-      };
-    },
-    props: {
-      id: {
-        type: Number,
-        default: 0,
+        title: String,
+        comments: [],
+        description: String,
+        ingredients: [],
+        owner: String,
+        preparationtime: Number,
+        rating:Number,
+        tags: [],
+        preparation: String
       }
+    },
+    methods: {
+      setData() {
+        fb.getRecipeById('Koek').then((data) =>
+        {
+          this.title = "Koek";
+          this.comments = data.comments;
+          this.description = data.description;
+          this.ingredients = data.ingredients;
+          this.owner = data.owner;
+          this.preparationtime = data.preparationtime;
+          this.rating = data.rating;
+          this.tags = data.tags;
+          this.preparation = data.preparation;
+        })
+
+      }
+    },
+    created() {
+      this.setData();
     }
   }
 </script>
@@ -181,6 +178,7 @@
   .ingredient-checkbox {
     margin-bottom: 3px;
     border-radius: 3px !important;
+    color: #E4A428 !important;
   }
 
   .instructions {
