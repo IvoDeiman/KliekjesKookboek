c2110a1820fd79df431c4fd3c2945c3682fa7d21
DPijlman <dannypijlman@gmail.com>
2024-01-15 10:03:31 +0100

    About page update
    Needs styling, but contains info now

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,39 @@
 <template>
-  <h1>About us</h1>
+  <div style="padding-top: 100px">
+    <h1 style="font-size: 70px">Wie zijn wij</h1>
+    <p>Even voorstellen, wij zijn het team achter Het Kliekjes Kookboek.
+    Wij zijn een jonge start-up opgericht door een groep jonge gemotiveerde
+    studenten van de Hanzehogeschool te Groningen.
 
+    Wij hebben elkaar leren kennen op de opleiding HBO-ICT toen we in jaar 4
+    in dezelfde klas kwamen. Wat begon als een idee voor een project is inmiddels
+    uitgegroeid tot werkelijkheid en werken wij full-time met enthousiasme en passie aan ons product.</p>
+
+    <h1 style="font-size: 70px">Wat is ons product</h1>
+    <p>Het Kliekjes Kookboek is een online receptenboek. Deze kan gebruikt worden
+    om nieuwe recepten te leren, informatie in te winnen of te filteren op overgebleven
+    kliekjes zodat deze in plaats van verspild worden gebruikt gaan worden in een
+    van de vele heerlijke recepten die wij aanbieden.</p>
+
+    <h1 style="font-size: 70px">Waarom</h1>
+    <p>"Gemiddeld verspilden Nederlanders in 2022 thuis 33,4 kg eten per persoon. Binnen de hele
+      voedselketen zijn van boer tot bord weliswaar veelbelovende initiatieven om verspilling
+      tegen te gaan, maar meer inzet is nodig. Nog altijd wordt ongeveer een kwart van
+      al het voedsel in Nederland verspild." Bron ~rijksoverheid</p><br>
+      <p>"De gemiddelde Nederlander maakt in een heel jaar tijd week in week uit dezelfde 7 recepten" Bron ~Adam Hasanin
+      </p><br>
+    <p>
+      Omdat wij van Het Kliekjes Kookboek vooruit willen kijken naar de toekomst pakken wij graag het probleem van voedselverspilling aan.
+      Zoals eerder genoemd is het mogelijk om op onze site met overgebleven kliekjes recepten te vinden.
+      Verder vinden wij het jammer om te lezen dat Nederlanders een relatief kleine culineare horizon hebben.
+      Ook hierbij willen wij ons van Het Kliekjes Kookboek inzetten om dit getal te veelvoudigen en zo de mensen kennis
+      te laten maken met andere heerlijke keuken!</p>
+
+    <h1 style="font-size: 70px">Motto</h1>
+    <p>Carpe diem sugondese nuts lmao</p>
+    <br>
+    <br>
+  </div>
 </template>
 
 <script>
