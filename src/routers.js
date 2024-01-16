b634f3dead47a431314a6086e86ba5477b57a612
Zettios <77236807+Zettios@users.noreply.github.com>
2024-01-16 13:52:46 +0100

    Start to AddRecipe.vue (adding recipes)

--- src/routers.js
+++ src/routers.js
@@ -3,10 +3,10 @@ import DefaultLayout from "@/components/DefaultLayout.vue";
 import Home from "@/views/Home.vue";
 import Recipes from "@/views/recipe/Recipes.vue";
 import RecipeDetail from "@/views/recipe/RecipeDetail.vue"
+import AddRecipe from "@/views/recipe/AddRecipe.vue"
 import About from "@/views/About.vue";
 import MyAccount from "@/views/MyAccount.vue";
 import Test from "@/views/test.vue";
-import RecipeInfo from "@/views/recipe/RecipeInfo.vue";
 import PageNotFound from "@/views/PageNotFound.vue";
 
 const routes = [
@@ -20,11 +20,6 @@ const routes = [
                 path: "/",
             },
             {
-              name: "RecipeInfo",
-              component: RecipeInfo,
-              path: "/recipe-info/:id"
-            },
-            {
                 name: "Recipes",
                 component: Recipes,
                 path: "/recipes",
@@ -33,6 +28,11 @@ const routes = [
                 name: "RecipesDetails",
                 component: RecipeDetail,
                 path: "/recipes-details/:id",
+            },
+            {
+                name: "AddRecipe",
+                component: AddRecipe,
+                path: "/add-recipes",
             },
             {
                 name: "About us",
---
+++ src/views/recipe/AddRecipe.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="upper-page-background"></div>
+    <div class="add-recipe-container">
+        <form>
+            Recept naam: <input type="text"><br>
+            Recept quote: <input type="text"><br>
+            Chef naam: <input type="text"><br>
+            Bereidings tijd: <input type="text"><br>
+            Ingrediënten:<br>
+            <select name="ingredienten">
+                <option value="tag1">Tag 1</option>
+                <option value="tag2">Tag 2</option>
+                <option value="tag3">Tag 3</option>
+            </select><br>
+            Instructies: <br>
+            <div class="instructions-container">
+                <textarea rows="5" cols="33">This is the default comment...</textarea> <br>
+                <textarea rows="5" cols="33">This is the default comment...</textarea> <br>
+                <textarea rows="5" cols="33">This is the default comment...</textarea> <br>
+            </div>
+            <button class="add-instruction-button">Voeg instructie toe</button>
+        </form>
+    </div>
+</template>
+  
+<script>
+//import FirebaseService from "@/services/FirebaseService";
+
+//const fb = new FirebaseService();
+
+export default {
+    name: "AddRecipe",
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+    },
+};
+</script>
+  
+<style>
+  .upper-page-background {
+    background: linear-gradient(240deg, rgba(255, 206, 112, 0.30) 30.28%, rgba(0, 0, 0, 0.00) 88.36%), #E4A428;
+    height: 368px;
+  }
+
+  .add-recipe-container {
+
+  }
+
+  .instructions-container {
+
+  }
+
+  .add-instruction-button {
+    background: #E4A428;
+  }
+</style>
